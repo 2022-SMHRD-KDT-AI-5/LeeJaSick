0956b4835bbb585b85815e6616293e84475bd280
smhrd <smhrd@DESKTOP-POV7T8B>
2022-05-20 16:30:05 +0900

    테스트커밋

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -2,9 +2,10 @@
 public class Test {
 
 	public static void main(String[] args) {
-
+		
 		System.out.println("첫 번째 커밋!");
-		System.out.println("두 번째 커밋!!");
+		System.out.println("테스트");
+		
 	}
 
 }
